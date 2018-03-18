7b633fe28777048385eb7004a33a1bd4b1fbaf5f
Ben Hutchison <brhutchison@gmail.com>
2018-03-18 17:41:01 +1100

    improved tag method. travis config

--- shared/src/main/scala/shapelessx/package.scala
+++ shared/src/main/scala/shapelessx/package.scala
@@ -1,1 +1,15 @@
-package object shapelessx
+import shapeless.tag
+import shapeless.tag.@@
+
+package object shapelessx {
+
+  object tagValue {
+    def apply[T](t: T) = new TaggedValue[T](t)
+    class TaggedValue[T](t: T) {
+      def @@[U]: T @@ U = tag[U].apply[T](t)
+    }
+  }
+
+}
+
+
